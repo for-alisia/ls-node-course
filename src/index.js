83c23480c6ded9dad58cb2dcbd904ce94606bd17
Elena Romanova <for-alisia@yandex.ru>
2020-05-29 17:42:21 +0500

    Started homework_2

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
+const http = require('http');
+require('dotenv').config();
 
+const interval = process.env.INTERVAL;
+const duration = process.env.DURATION;
+const port = process.env.PORT;
+
+const consoleOutput = () => {
+  const timer = setInterval(() => {
+    const date = new Date(new Date().toUTCString());
+    console.log(date);
+  }, interval);
+
+  return timer;
+};
+
+const reqHandler = (req, res) => {
+  const timer = consoleOutput();
+  setTimeout(() => {
+    clearInterval(timer);
+    res.end(new Date().toUTCString());
+  }, duration);
+};
+
+const server = http.createServer(reqHandler);
+
+server.listen(port, (err) => {
+  if (err) {
+    return console.log('Error happened', err);
+  }
+
+  console.log(`server is listening on ${port}`);
+});
+
+console.log(interval, duration, port);
